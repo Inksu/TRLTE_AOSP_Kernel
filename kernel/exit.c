c9f5b543ef01f49299538e4c4d42001cbd7d7c65
crimsonthunder <crimsonthunder54@gmail.com>
2017-01-22 15:59:32 +0100

    fix build

--- kernel/exit.c
+++ kernel/exit.c
@@ -501,10 +501,7 @@ static void exit_mm(struct task_struct * tsk)
 	enter_lazy_tlb(mm, current);
 	task_unlock(tsk);
 	mm_update_next_owner(mm);
-
-	mm_released = mmput(mm);
-	if (mm_released)
-		set_tsk_thread_flag(tsk, TIF_MM_RELEASED);
+	mmput(mm);
 }
 
 /*
